402c80f7008a5a89327661efc2bafbd397774c3f
twyunting <yc6705a@american.edu>
2020-09-01 23:46:03 -0400

    add nrow and ncol

--- wk2/hw_wk2_Yunting.R
+++ wk2/hw_wk2_Yunting.R
@@ -1,5 +1,14 @@
-# 1. add libraries
+# add libraries
 library(tidyverse)
 library(readr)
-leadDF <- read_csv(file = "../data/lead.csv")
-print(leadDF)
+
+# read homework's data
+bloodlead <- read_csv(file = "../data/lead.csv")
+head(bloodlead)
+
+# How many individuals are in the data set?
+nrow(bloodlead)
+
+# How many variables are in this data set?
+ncol(bloodlead)
+
